812c48aa373a4b07e1e4a1eae5f448fd51f56663
Victor CUAU <victor.cuau@hotmail.fr>
2018-12-03 16:58:08 +0100

    Q4 + début Q5

--- downloader/src/downloader/ui/DownloadTask.java
+++ downloader/src/downloader/ui/DownloadTask.java
@@ -2,22 +2,47 @@ package downloader.ui;
 
 import downloader.fc.Downloader;
 import javafx.application.Platform;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 
-public class DownloadTask extends ProgressBar {
+public class DownloadTask extends BorderPane {
 	Downloader downloader;
+	ProgressBar progressBar;
   Thread thread;
+  Label nomUrl;
+  Button buttonSuppr;
+  Button buttonPause;
+  Button buttonReprendre;
+  HBox buttonBar;
   
   DownloadTask(String URL){
   	downloader = new Downloader(URL);
-  	this.setMaxWidth(Double.MAX_VALUE);
+  	progressBar = new ProgressBar();
+  	progressBar.setMaxWidth(Double.MAX_VALUE + 10);
   	thread = new Thread(downloader);
+  	nomUrl = new Label(URL);
+  	
+  	Button buttonSuppr = new Button("X");
+  	Button buttonPause = new Button("||");
+  	Button buttonReprendre = new Button(">");
+  	buttonBar = new HBox();
+  	buttonBar.getChildren().addAll(buttonSuppr,buttonPause,buttonReprendre);
+  	buttonBar.setHgrow(progressBar, Priority.ALWAYS);
+  	buttonBar.setSpacing(1.5);
+  	
+  	this.setTop(nomUrl);
+  	this.setCenter(progressBar);
+  	this.setRight(buttonBar);
   	
   	// On abonne la ProgressBar aux mises à jour de la progression du téléchargement
   	downloader.progressProperty().addListener((obs, o, n) -> {
   		Platform.runLater(new Runnable() {
         public void run() {
-          setProgress(n.doubleValue());
+        	progressBar.setProgress(n.doubleValue());
         }
       });
     });
